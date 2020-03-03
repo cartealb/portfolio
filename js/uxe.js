8f440a460df04541727c6d2fc914e060ba25ae3e
AJ Carter <ajcarter@AJs-MacBook-Pro.local>
2020-03-02 20:03:09 -0500

    added parrelax on header

--- js/uxe.js
+++ js/uxe.js
@@ -19,12 +19,44 @@ function go_to_topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-// https://codepen.io/nickcil/pen/sfutl
-$(window).scroll(function() {
-    var val = window.matchMedia('(prefers-reduced-motion: reduce)');
-    if (!(val.matches)) {
-        $("#intro").css("opacity", 1 - $(window).scrollTop() / 450);
-        $("#nav_bar").css("opacity", 1 - $(window).scrollTop() / 450);
-    }
+// // https://codepen.io/nickcil/pen/sfutl
+// $(window).scroll(function() {
+//     var val = window.matchMedia('(prefers-reduced-motion: reduce)');
+//     if (!(val.matches)) {
+//         $("#intro").css("opacity", 1 - $(window).scrollTop() / 450);
+//         $("#nav_bar").css("opacity", 1 - $(window).scrollTop() / 450);
+//     }
 
+// });
+
+window.addEventListener('scroll', function(e) {
+    const target = document.querySelector('#me_pic');
+
+    let scrolled = window.pageYOffset;
+    let rate = scrolled * 0.45;
+    target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
+});
+
+window.addEventListener('scroll', function(e) {
+    const target = document.querySelector('#title_me');
+
+    let scrolled = window.pageYOffset;
+    let rate = scrolled * 0.45;
+    target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
+});
+
+window.addEventListener('scroll', function(e) {
+    const target = document.querySelector('#nav_bar');
+
+    let scrolled = window.pageYOffset;
+    let rate = scrolled * 0.45;
+    target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
+});
+
+window.addEventListener('scroll', function(e) {
+    const target = document.querySelector('#nav_bar');
+
+    let scrolled = window.pageYOffset;
+    let rate = scrolled * 0.45;
+    target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
 });
